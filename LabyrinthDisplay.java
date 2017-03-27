37be8fae485a9093189a04d2853c0208dbe35591
Bedredin Celina <Cobure@users.noreply.github.com>
2017-03-27 08:55:22 +0200

    Update LabyrinthDisplay.java

--- LabyrinthDisplay.java
+++ LabyrinthDisplay.java
@@ -1,15 +1,3 @@
-/*
-import javax.swing.JFrame;
-import java.awt.Toolkit;//used for getScreenSpecs0
-import java.awt.Dimension;//used for getScreenSpecs0
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.awt.Image;
-import java.awt.GraphicsEnvironment;//used for getScreenSpecs
-import java.awt.GraphicsDevice;//used for getScreenSpecs1
-import java.util.Timer;
-import java.awt.event.*;
-*/
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
@@ -20,8 +8,6 @@ import java.lang.Math;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.util.Random;
-//import java.awt.KeyEventDispatcher;
-//import java.awt.KeyboardFocusManager;
 
 public class LabyrinthDisplay extends JFrame implements ActionListener, KeyListener{
 	
@@ -38,20 +24,23 @@ public class LabyrinthDisplay extends JFrame implements ActionListener, KeyListe
 	private int time;
 	private int FPS=0;
 	
+	
+	
 	private boolean [][] MAP;
 	private Player player;
+	private int DIFFICULTY = 10;
+	private int step=10;
 	
 	public LabyrinthDisplay(){
 		//super("LABYRINTH");
 		getScreenSpecs();
 		setTimer();
-		generateMap();
-		//if(isFullScreen && isFullScreenSupported()) setFullScreenWindow(Window w); //for exclusive full mode - https://docs.oracle.com/javase/tutorial/extra/fullscreen/exclusivemode.html		
+		//generateMap();
+		Maze map = new Maze (DIFFICULTY);
+		MAP = map.maze;
+		map.draw();
 		this.setLayout(null);
 		//this.setIconImage(Image image); //details
-		//this.setExtendedState(JFrame.MAXIMIZED_BOTH); does not require getScreenSpecs
-		//SCREEN_WIDTH=800; //testing size
-		//SCREEN_HEIGHT=600;
 		this.setSize(SCREEN_WIDTH,SCREEN_HEIGHT);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -66,10 +55,10 @@ public class LabyrinthDisplay extends JFrame implements ActionListener, KeyListe
 		
 		myTimer = new Timer(TPS_TIMER_MS,this);
 		time = 0;
+		int startY = map.getStartY();
+		player = new Player(0,startY*SCREEN_HEIGHT/(DIFFICULTY*9),0);
 		
-		player = new Player(0,0,0);
-		
-		radius = 5;
+		radius = 1;
 		
 		//Mask = new DistanceFilter(500,SCREEN_WIDTH,SCREEN_HEIGHT);
 		if(MASKING)Mask = new DistanceFilter(100*radius,SCREEN_WIDTH,SCREEN_HEIGHT);
@@ -96,16 +85,6 @@ public class LabyrinthDisplay extends JFrame implements ActionListener, KeyListe
 	}
 	
 	
-	
-	/*
-	private static void getScreenSpecs0(){ // gets screen resolution
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		SCREEN_WIDTH = (int)screenSize.getWidth();
-		SCREEN_HEIGHT = (int)screenSize.getHeight();
-		//System.out.println(width+"X"+height);
-	}
-	*/
-	
 	private static void getScreenSpecs(){ //gets all screen specs
 		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		GraphicsDevice[] devices = env.getScreenDevices();
@@ -116,16 +95,6 @@ public class LabyrinthDisplay extends JFrame implements ActionListener, KeyListe
 			SCREEN_HEIGHT = device.getDisplayMode().getHeight();
 			SCREEN_REFRESH = device.getDisplayMode().getRefreshRate();
 			SCREEN_BIT_RATE = device.getDisplayMode().getBitDepth();
-			
-			/*
-			System.out.println("");
-			System.out.println("Screen Number [" + (sequence++) + "]");
-            System.out.println("Width       : " + SCREEN_WIDTH);
-            System.out.println("Height      : " + SCREEN_HEIGHT);
-            System.out.println("Refresh Rate: " + SCREEN_REFRESH);
-            System.out.println("Bit Depth   : " + SCREEN_BIT_RATE);
-            System.out.println("");
-            */
         }
 		
 	}
@@ -148,18 +117,17 @@ public class LabyrinthDisplay extends JFrame implements ActionListener, KeyListe
 		Graphics buff = buffer.getGraphics();
 		buff.drawImage(background,0,0,this);//has a fluidity effect without a wallpaper due to partial transparency of the walls
 		
-		for(int i=0;i<16;i++){ //paint rectangles in 16:9 aspect ratio
-			for(int k=0;k<+9;k++){
-				if(MAP[i][k]==false){
-					//buff.setColor(Color.WHITE);
-					buff.setColor(new Color(255,255,255,127)); //50% transparent white
+		for(int i=0;i<(int)(16*DIFFICULTY);i++){ //paint rectangles in 16:9 aspect ratio
+			for(int k=0;k<(int)(9*DIFFICULTY);k++){
+				if(MAP[k][i]==false){
+					buff.setColor(new Color(0,0,0,127)); //50% transperent Black
 					
 				}else{
-					//buff.setColor(Color.BLACK);
-					buff.setColor(new Color(0,0,0,127)); //50% transperent Black
+					buff.setColor(new Color(255,255,255,127)); //50% transparent white
+					
 				}
-				double square_width=(((double)SCREEN_WIDTH)/16.);
-				double square_height=(((double)SCREEN_HEIGHT)/9.);
+				double square_width=(((double)SCREEN_WIDTH)/(16.*((double)DIFFICULTY)));
+				double square_height=(((double)SCREEN_HEIGHT)/(9.*((double)DIFFICULTY)));
 				//buff.drawRect((int)((double)i*square_width),(int)((double)k*square_height),(int)(square_width),(int)(square_height));//reduce leftover from redraw, in the case of no background being repainted. Else it makes too bright/dark lines
 				buff.fillRect((int)((double)i*square_width),(int)((double)k*square_height),(int)(square_width),(int)(square_height));
 			}
@@ -207,19 +175,19 @@ public class LabyrinthDisplay extends JFrame implements ActionListener, KeyListe
 		}
 		else if(e.getKeyCode()== KeyEvent.VK_UP){
 			System.out.println("UP Pressed!");
-			if(player.y>0) player.y--;
+			if(player.y>0) player.y-=step;
 		}
 		else if(e.getKeyCode()== KeyEvent.VK_DOWN){
 			System.out.println("DOWN Pressed!");
-			if(player.y<(SCREEN_HEIGHT-20)) player.y++;//switch 20 by player y dimension (to be implemented)
+			if(player.y<(SCREEN_HEIGHT-20)) player.y+=step;//switch 20 by player y dimension (to be implemented)
 		}
 		else if(e.getKeyCode()== KeyEvent.VK_LEFT){
 			System.out.println("LEFT Pressed!");
-			if(player.x>0) player.x--;
+			if(player.x>0) player.x-=step;
 		}
 		else if(e.getKeyCode()== KeyEvent.VK_RIGHT){//switch 20 by player x dimension (to be implemented)
 			System.out.println("RIGHT Pressed!");
-			if(player.x<(SCREEN_WIDTH-20)) player.x++;
+			if(player.x<(SCREEN_WIDTH-20)) player.x+=step;
 		}
 		else if(e.getKeyCode()== KeyEvent.VK_ENTER){
 			System.out.println("ENTER Pressed!");
